eb7aa41a8753644011fdffe6d6d3370cc6a9b260
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-05-12 13:30:07 +0700

    Scaffold test project

---
+++ test/web-app.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace web_app
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Hello from test");
+        }
+    }
+}
